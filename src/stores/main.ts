3ae2391a12ed5a5a100e21d440a663dd1f7941eb
Chansol Jeong <mutongzang@gmail.com>
2024-03-20 00:33:27 +0900

    문제 텍스트에도 강조표시 추가

--- src/stores/main.ts
+++ src/stores/main.ts
@@ -249,13 +249,14 @@ export const useMainStore: StoreDefinition = defineStore("main", () => {
       "Elasticsearch는 <b>REST API</b>를 통해 접근할 수 있습니다.",
       "Elasticsearch의 모든 쿼리와 결과는 <b>JSON</b> 형태로 이루어져 있습니다.",
       `기본적으로 <b>${JSON.stringify({ query: {} }, null, 4)}</b> 구조를 가지고 있습니다.`,
-      "<i>제출을 클릭해보세요.(또는 Ctrl+Enter를 입력하세요.)</i>"
+      "<i><b>format</b> 버튼을 누르면 <b>JSON</b> 포맷으로 변경해줍니다.</i>",
+      "<i><b>제출</b>을 클릭해보세요.(또는 Ctrl+Enter를 입력하세요.)</i>"
     ],
     2: [
       "Elasticsearch의 텍스트 검색은 크게 <b>유사일치</b>와 <b>완벽일치</b>로 나뉩니다.",
       "match 쿼리는 검색어에 대해 분석과정을 수행해 텍스트를 <b>토큰화</b>한 뒤 검색에 사용합니다.",
       "match 쿼리에서 'Eddie Underwood'와 같은 값은 'Eddie', 'eddie', 'Underwood', 'underwood 4가지 검색어 전부로 검색가능합니다.",
-      "<i>match 쿼리로 customer_full_name 필드가 'Bailey'인 문서를 검색하세요. </i>"
+      "<i><b>match</b> 쿼리로 <b>customer_full_name</b> 필드가 <b>Bailey</b>인 문서를 검색하세요. </i>"
     ],
     3: [
       "<b>Term 쿼리는 정확한 값을 필드에서 찾아 문서를 검색</b>합니다.",
@@ -265,7 +266,7 @@ export const useMainStore: StoreDefinition = defineStore("main", () => {
       "<b>(중요)</b> Elasticsearch 역색인으로 작동합니다. 'Eddie Underwood'로 검색하였을 경우 text 타입으로 저장되었을 경우 역색인에 'Eddie Underwood'이 없습니다.",
       "<b>(중요)</b> 역색인에 존재하는 데이터는 'Eddie'와 'Underwood'만이 있습니다",
       "<b>(중요)</b> 일반적으로 text 타입으로 지정하는 필드는 keyword라는 하위필드가 존재합니다. <b>.keyword</b>로 접근가능합니다.",
-      "<i>customer_full_name.keyword를 이용하여 customer_full_name 필드가 Eddie Underwood인 문서를 검색하세요.</i>"
+      "<i><b>customer_full_name.keyword</b>를 이용하여 <b>customer_full_name</b> 필드가 <b>Eddie Underwood</b>인 문서를 검색하세요.</i>"
     ],
     4: [
       "<b>Range 쿼리는 지정된 범위 내의 값들을 가진 문서</b>를 찾는 데 사용됩니다.",
@@ -273,7 +274,7 @@ export const useMainStore: StoreDefinition = defineStore("main", () => {
       "숫자, 날짜, 문자열 등 다양한 타입의 필드에 적용할 수 있습니다.",
       "<b>gte</b> : 이상, <b>lte</b> : 이하",
       "<b>gt</b> : 초과, <b>lt</b> : 미만",
-      "<i>taxful_total_price 필드의 값이 100 이상인 문서를 검색하세요.</i>"
+      "<i><b>taxful_total_price</b> 필드의 값이 <b>100 이상</b>인 문서를 검색하세요.</i>"
     ],
     5: [
       "<b>Boolean 쿼리는 논리 연산자(AND, OR, NOT)를 사용해 여러 검색 조건을 결합</b>합니다.",
@@ -282,7 +283,7 @@ export const useMainStore: StoreDefinition = defineStore("main", () => {
       "Boolean 쿼리의 하위 필드들은 항상 <b>배열형태의 value</b>만 받습니다.",
       "<b>must</b> 쿼리는 지정된 조건을 만족하는 문서만 검색 결과에 포함됩니다.",
       "<b>must_not</b> 쿼리는 지정된 조건을 만족하지 않는 문서만 검색 결과에 포함됩니다.",
-      "<i>customer_full_name 필드에서 'Mary'가 포함되어 있으면서 'Bailey'는 포함되지 않는 문서를 검색하세요.</i>"
+      "<i><b>customer_full_name</b> 필드에서 <b>Mary</b>가 포함되어 있으면서 <b>Bailey</b>는 포함되지 않는 문서를 검색하세요.</i>"
     ],
     6: [
       "<b>filter 쿼리는 검색 결과를 필터링하여 특정 기준을 만족하는 문서</b>만을 결과로 반환합니다.",
@@ -290,7 +291,7 @@ export const useMainStore: StoreDefinition = defineStore("main", () => {
       "결과 값은 must와 같습니다. 대신 filter 쿼리는 score를 계산하지 않습니다.",
       "score 계산에는 키워드의 빈도, 문서의 길이, 쿼리와의 관련성 등 여러 요소를 기반으로 합니다.",
       "<b>(중요)</b> filter 쿼리의 결과는 점수 계산이 생략된 만큼 must 쿼리보다 빠르며 <b>캐싱</b>이 적용됩니다.",
-      "<i>email 필드의 값이 정확히 'gwen@butler-family.zzz'이면서 taxful_total_price 필드 값이 100 이상인 문서를 검색하세요.</i>"
+      "<i><b>email</b> 필드의 값이 정확히 <b>gwen@butler-family.zzz</b>이면서 <b>taxful_total_price</b> 필드 값이 <b>100 이상</b>인 문서를 검색하세요.</i>"
     ],
     7: [
       "<b>should 쿼리는 주어진 조건 중 하나 이상을 만족</b>하는 문서를 찾습니다.",
@@ -298,36 +299,39 @@ export const useMainStore: StoreDefinition = defineStore("main", () => {
       "must나 must_not 조건과 함께 사용하여 유연한 검색 조건을 구성할 수 있습니다.",
       "<b>minimum_should_match</b> 값을 이용하여 <b>최소 n개 이상의 조건</b>을 만족하는 문서를 검색할 수 있습니다",
       "<b>(중요)</b> minimum_should_match은 should만 있다면 기본값이 1이고 , <b>다른 bool 쿼리가 있다면 기본값이 0입니다(검색결과에 영향x)</b>",
-      "<i>1. customer_full_name 필드 값이 Oliver Rios 이거나<br>2. day_of_week 필드값이 'Monday'이거나<br>3. taxful_total_price 필드의 값이 1000 초과<br>위 조건중 2개이상을 만족하는 문서를 검색하세요.</i>"
+      "<i>1. <b>customer_full_name</b> 필드 값이 <b>Oliver Rios</b> 이거나</i>",
+      "<i>2. <b>day_of_week</b> 필드값이 <b>Monday</b>이거나</i>",
+      "<i>3. <b>taxful_total_price</b> 필드의 값이 <b>1000 초과</b></i>",
+      "<i> 위 조건중 2개이상을 만족하는 문서를 검색하세요.</i>"
     ],
     8: [
       "must, must_not, filter, should 쿼리는 Boolean 쿼리를 조건으로 받을 수 있습니다.",
       "중첩된 Boolean 쿼리를 사용하면 여러 Boolean 쿼리를 결합하여 더 복잡한 검색 조건을 만들 수 있습니다.",
       "예를 들어, 특정 단어를 포함하면서 다른 단어는 포함하지 않는 문서를 찾되, 추가 조건으로 특정 범위의 날짜를 가진 문서를 찾는 것과 같은 조건을 정의할 수 있습니다.",
       "<i>아래와 같은 조건의 문서를 검색하세요.</i>",
-      "<i>1.total_quantity 값이 4 이상이거나</i>",
-      "<i>2.taxful_total_price의 값이 200 이상이면서 day_of_week가 'Monday'인 문서</i>",
+      "<i>1.<b>total_quantity</b> 값이 <b>4 이상</b>이거나</i>",
+      "<i>2.<b>taxful_total_price</b>의 값이 <b>200 이상</b>이면서 <b>day_of_week</b>가 <b>Monday</b>인 문서</i>",
       "<i class='gothic-font'><b>(정리)</b> (total_quantity>=4) || (taxful_total_price>=200 && day_of_week=\"Monday\")</i>"
     ],
     9: [
       "<b>Terms 쿼리는 여러 개의 정확한 값을 동시에 검색</b>할 수 있습니다.",
       "예를 들어, 여러 개의 특정 ID나 키워드를 한 번에 검색하고 싶을 때 유용합니다.",
       "Terms 쿼리는 <b>{[fieldname]:Array[]}</b> 형태의 Object를 받으며, term과 마찬가지로 정확히 일치하는 값을 찾습니다. text 타입을 검색할 때는 <b>.keyword</b>를 붙여줘야 합니다.",
-      "<i>manufacturer 필드의 값이 'Elitelligence' 또는 'Pyramidustries'인 문서를 검색하세요</i>"
+      "<i><b>manufacturer</b> 필드의 값이 <b>Elitelligence</b> 또는 <b>Pyramidustries</b>인 문서를 검색하세요</i>"
     ],
     10: [
       "<b>Wildcard 쿼리를 사용하면 패턴 매칭을 통해 검색</b>을 수행할 수 있습니다.",
       "예를 들어, 텍스트의 일부만 알고 있을 때, <b>*</b>나 <b>?</b>와 같은 와일드카드 문자를 사용하여 검색할 수 있습니다.",
-      "<i>user 필드의 값이 'r'로 시작하면서 중간에 'bb'가 들어간 문서를 검색하세요</i>"
+      "<i><b>user</b> 필드의 값이 <b>r</b>로 시작하면서 중간에 <b>bb</b>가 들어간 문서를 검색하세요</i>"
     ],
     11: [
       "<b>Exists 쿼리는 특정 필드에 값이 존재하는 문서</b>만을 찾을 때 사용합니다.", "이는 필드의 존재 여부만을 판단하여 해당 필드가 있는 문서를 검색 결과로 반환합니다.",
       "<b>exists_not은 없습니다.</b> 필드가 존재하지 않는 경우의 검색을 원할 경우 must_not에 exists를 사용해야합니다.",
-      "<i>email 필드가 있는 문서를 검색하세요.</i>"
+      "<i><b>email</b> 필드가 있는 문서를 검색하세요.</i>"
     ],
     12: [
       "<b>multi_match 쿼리는 여러 필드에서 동시에 검색어에 대해 검색</b>합니다.", "이는 하나의 검색어를 다양한 필드에서 찾고 싶을 때 사용되며, 여러 필드에 걸쳐 있는 정보를 통합적으로 검색할 수 있게 해줍니다.",
-      "<i>'customer_first_name', 'customer_last_name', 'email' 필드 중에서 'Eddie' 값을 포함하는 문서를 검색하세요.</i>"
+      "<i><b>customer_first_name</b>, <b>customer_last_name</b>, <b>email</b> 필드 중에서 <b>Eddie</b> 값을 포함하는 문서를 검색하세요.</i>"
     ],
     13: [
       "<b>'from'과 'size' 파라미터는 검색 결과의 페이징을 처리</b>할 때 사용됩니다.",
@@ -339,21 +343,21 @@ export const useMainStore: StoreDefinition = defineStore("main", () => {
       "_source 필드는 false, 문자열 배열과 'include'와 'exclude'로 이루어진 Object로도 설정 가능합니다.",
       "false로 지정할 경우 검색결과에 _source 데이터가 포함되지 않습니다. 배열형태로 전달시 <b>wildcard</b> 형태로 전달가능합니다.",
       "Object 형태로 입력할 경우 <b>include</b> 배열로 받은 필드만이 출력되고, <b>exclude</b> 배열로 받은 필드는 제외됩니다.",
-      "<i>customer_으로 시작하는 필드는 포함하지만, _id로 끝나는 필드는 검색결과에 포함되지 않도록 검색하세요</i>"
+      "<i><b>customer_</b>으로 시작하는 필드는 포함하지만, <b>_id</b>로 끝나는 필드는 검색결과에 포함되지 않도록 검색하세요</i>"
     ],
     15: [
       "<b>sort 옵션을 사용하면 검색 결과의 정렬 순서를 지정</b>할 수 있습니다.",
       "예를 들어, 날짜나 가격과 같은 필드를 기준으로 오름차순 또는 내림차순으로 정렬할 수 있습니다.",
       "sort에는 <b>script</b>가 사용이 가능합니다.",
       "오름차는 <b>asc</b>, 내림차는 <b>desc</b>로 표현됩니다.",
-      "<i>order_date 필드를 기준으로 내림차순으로 정렬해서 검색하세요.</i>"
+      "<i><b>order_date</b> 필드를 기준으로 내림차순으로 정렬해서 검색하세요.</i>"
     ],
     16: [
       "<b>Aggregations 쿼리는 데이터에 대한 통계적 분석을 제공</b>하여, 여러 문서의 데이터를 요약, 그룹화, 혹은 집계하여 보여줍니다.",
       "이를 통해 대량의 데이터에서 유용한 정보를 추출하고, 데이터의 패턴이나 트렌드를 파악할 수 있습니다.",
       "aggs 필드는 query 필드의 조건에 해당하는 문서들에 대한 통계입니다.",
       "Metrics Aggregations에는  <b>min</b>, <b>max</b>, <b>sum</b>, <b>avg</b>등이 있습니다.",
-      "<i>챕터 16부터는 문제가 아니라 기능설명입니다!</i>"
+      "<i>챕터 16부터는 문제가 아니라 <b>기능설명</b>입니다!</i>"
     ],
     17: [
       "<b>Bucket Aggregation은 주어진 조건으로 분류된 버킷들을 만들고, 각 버킷에 소속되는 문서들을 모아 그룹으로 구분</b>하는 것입니다",
